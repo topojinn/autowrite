2fb446741bd3fb6031c8a4bc81b2e45860e0c25e
Erma <topojinn2@gmail.com>
2026-08-27 14:15:58 +0200

    edit code

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+#set delay
+delay = 0.10
+
+def exit_error():
+    config.exit(1)
+
 try:
   import sys as config
 except ImportError:
   print("error: module import fail")
+  exit_error()
 except ModuleNotFoundError:
   print("error: module not imported")
+  exit_error()
+
 
 try:
   import time
 except ImportError:
     print("error: module import fail")
+    exit_error()
 except ModuleNotFoundError:
     print("error: module not imported")
+    exit_error()
+
 
 print("with text you want to automate?")
 repl = input()
@@ -23,6 +35,6 @@ for digit in repl:
     print(digit, end="")
     config.stdout.flush()
     
-    time.sleep(0.05)
+    time.sleep(delay)
 
 print()
